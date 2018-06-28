3f59ac2acb020d2eabc98fbd6cf434e03bb59363
h3xar0n <aron@eideldev.com>
2018-06-28 13:58:40 -0700

    Minesweeper passes all green, but need to improve use of ES6

--- minesweeper.js
+++ minesweeper.js
@@ -1,32 +1,94 @@
 function minesweeper(matrix) {
-    // initialize number matrix
-    let numbermatrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+    const board = [];
+    const matrixLength = matrix.length;
     
-    console.log(numbermatrix);
+    // figure out how to do with forEach
+    // matrix.forEach((row) => {
+    //     let rowLength = row.length;
+    //     let newRow = [];
+    //     row.forEach((cell) => {
+    //         newRow.push(clockCheck(matrix, matrixLength, row, rowLength, cell));
+    //     });
+    //     board.push(newRow);
+    // })
     
-    for (let row = 0; row < matrix.length; row++) {
-        for (let cell = 0; cell < matrix[row].length; cell++) {
-            if (matrix[row][cell] === true) {
-                console.log(row + ', ' + cell + ' :');
-                if (cell > 0) {
-                    numbermatrix[row][cell - 1]++;
-                    console.log(numbermatrix);
-                }
-                if (cell < (matrix[row].length - 1)) {
-                    numbermatrix[row][cell + 1]++;
-                    console.log(numbermatrix);
-                }
-                if (row > 0) {
-                    numbermatrix[row - 1][cell]++;
-                    console.log(numbermatrix);
-                }  
-                if (row < (matrix.length - 1)) {
-                    numbermatrix[row + 1][cell]++;
-                    console.log(numbermatrix);
-                }
-            }
+    for (let i = 0; i < matrixLength; i++) {
+        let oldRow = matrix[i];
+        let rowLength = oldRow.length;
+        let newRow = [];
+        for (let j = 0; j < rowLength; j++) {
+            newRow.push(clockCheck(matrix, matrixLength, i, rowLength, j));
+        }
+        board.push(newRow);
+    }
+    return board;
+}
+
+function clockCheck(matrix, matrixLength, matrixIndex, rowLength, rowIndex) {
+    
+    let cellValue = { value: 0 };
+    
+    // check conditions of surroundings
+    const hasRowAbove = matrixIndex > 0;
+    const hasRowBelow = matrixIndex < matrixLength - 1;
+    const hasColumnLeft = rowIndex > 0;
+    const hasColumnRight = rowIndex < rowLength - 1;
+    
+    // assign indices for surroundings
+    const top = matrixIndex - 1;
+    const bottom = matrixIndex + 1;
+    const left = rowIndex - 1;
+    const right = rowIndex + 1;
+    
+    // check directly left
+    if (hasColumnLeft) {
+        cellCheck(matrix, cellValue, matrixIndex, left);
+    }
+    
+    // check directly right
+    if (hasColumnRight) {
+        cellCheck(matrix, cellValue, matrixIndex, right);
+    }
+    
+    // check all above
+    if (hasRowAbove) {
+        
+        //check directly above
+        cellCheck(matrix, cellValue, top, rowIndex);
+    
+        //check top-left
+        if (hasColumnLeft) {
+            cellCheck(matrix, cellValue, top, left);
+        }
+               
+        //check top-right
+        if (hasColumnRight) {
+            cellCheck(matrix, cellValue, top, right);
         }
     }
     
-    return numbermatrix;
+    // check all below
+    if (hasRowBelow) {
+        
+        //check directly below
+        cellCheck(matrix, cellValue, bottom, rowIndex);
+    
+        //check bottom-left
+        if (hasColumnLeft) {
+            cellCheck(matrix, cellValue, bottom, left);
+        }
+               
+        //check bottom-right
+        if (hasColumnRight) {
+            cellCheck(matrix, cellValue, bottom, right);
+        }
+    }
+    
+    return cellValue.value;
+}
+
+function cellCheck(matrix, cellValue, vertical, horizontal) {
+    if (matrix[vertical][horizontal]) {
+        cellValue.value++;
+    }
 }
