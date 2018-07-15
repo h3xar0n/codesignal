9ec03e2ab082b88dffd4851abf9ddc7368934afa
h3xar0n <aron@eideldev.com>
2018-07-15 12:53:31 -0700

    Put descriptions beneath methods

--- 1_intro/1_the_journey_begins/2_centuryFromYear/centuryFromYear.js
+++ 1_intro/1_the_journey_begins/2_centuryFromYear/centuryFromYear.js
@@ -1,5 +1,6 @@
 function centuryFromYear(year) {
-    // divide by 100 and round down to avoid remainder, e.g. 1999 and 1901 both give 19
-    // and then add 1 for the century, e.g. 1901 and 1999 were both in the 20th century
     return Math.floor((year - 1)/ 100) + 1
 }
+
+ // divide by 100 and round down to avoid remainder, e.g. 1999 and 1901 both give 19
+ // and then add 1 for the century, e.g. 1901 and 1999 were both in the 20th century
