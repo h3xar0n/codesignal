0386f812dfec56d1ed1bb3b5f2380e46ac57b91d
h3xar0n <aron@eideldev.com>
2018-07-15 13:38:12 -0700

    Refactor Python and Lua based on JS; use Math.floor instead of convoluted approach

--- 1_intro/1_the_journey_begins/2_centuryFromYear/centuryFromYear.lua
+++ 1_intro/1_the_journey_begins/2_centuryFromYear/centuryFromYear.lua
@@ -1,15 +1,3 @@
 function centuryFromYear(year)
-  local included = true
-  -- find years beyond increment of 100
-  local spare = year % 100
-  if spare > 0 then
-    included = false 
-  end
-  -- the actual century
-  local century = year / 100
-  -- if the year is not a perfect increment of 100, to get the correct century, do not count past the year. 
-  if included == true then
-    century = century - 1
-  end
-  return century - (spare / 100) + 1
+  return math.floor((year - 1) / 100) + 1
 end
--- 1_intro/1_the_journey_begins/2_centuryFromYear/centuryFromYear.py
+++ 1_intro/1_the_journey_begins/2_centuryFromYear/centuryFromYear.py
@@ -1,16 +1,2 @@
 def centuryFromYear(year):
-    included = True
-    
-    # find years beyond increment of 100
-    spare = year % 100
-    if spare > 0:
-        included = False
-    
-    # an actual century
-    century = year / 100
-    
-    # if the year is not a perfect increment of 100, to get the correct century, do not count past the year.
-    if included == True:
-        century = century - 1
-        
-    return century - (spare / 100) + 1
+    return math.floor((year - 1) / 100) + 1
